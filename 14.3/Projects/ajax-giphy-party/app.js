c0dbc22d9b843fc259c18a0829f2dc7d478e48da
SyscilyBrown <55116854+SyscilyBrown@users.noreply.github.com>
2022-01-25 21:33:30 -0500

    removing erroneous comments

--- 14.3/Projects/ajax-giphy-party/app.js
+++ 14.3/Projects/ajax-giphy-party/app.js
@@ -1,27 +1,4 @@
-// const searchTerm = document.getElementById("searchTerm")
 
-// async function addGif (searchTerm){
-//   const urls = []
-//   const res = await axios.get("https://api.giphy.com/v1/gifs/search?", {
-//     params: {
-//       q: searchTerm,
-//       api_key: "yDuST6uQY24qwi4JvrttR1xVk9Xn1aRL"
-//     }
-//   });
-//   for(let url of res.data.data){
-//     urls.push(res.data.data[0].images.original.url)
-//     //returning every url
-//   }
-//   randomGif = urls[Math.floor(Math.random()*urls.length)]
-//   // newGif = document.createElement("img")
-//   // newGif.src = randomGif
-//   // console.log(newGif.value)
-//   console.log(randomGif)
-//   newGif = document.createElement("img")
-//   newGif.setAttribute("src", `${randomGif}`)
-//   document.body.appendChild(newGif)
-
-// }
 
 const form = document.getElementById("form")
 form.addEventListener("submit", async function(event) {
@@ -55,14 +32,6 @@ removeBtn.addEventListener("click", function(event){
   event.preventDefault(); 
   $(".gifs").remove()
 })
-//document.getElementById("submit-button").addEventListener("click", addGif)
-
-
-//add gif to page from click event listener 
-//async function appendGif(){
-//const button = document.getElementById("submit-button")
-//button.addEventListener("click", appendGif)
-//}
 
 
   /* 
@@ -72,12 +41,4 @@ removeBtn.addEventListener("click", function(event){
   append said gif to doc
   clear input 
   event listener from click on remove button to remove all gif 
-    - give images class to remove all 
-  
-getUserInput() => {
- 1) getData(userInput)
- const data = getData(userInput)
- 2) randomImage = getRandomImage(data)
- 3) addImage(randomImage)
-}
-  */
+    - give images class to remove all */
